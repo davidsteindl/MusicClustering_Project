5ccd92f2bc9e906bcfcc3cfc7d4566b5ca500075
David Steindl <davidsteindl0@gmail.com>
2025-11-04 23:27:09 +0100

    Added  jupyter notebook 03_cluster_analysis, with logic to evaluate the clustered data & added evaluation_utils.py with actual logic for creating the plots & added genre_groups.py with the grouped genre labels from the original data

---
+++ src/config/genre_groups.py
@@ -0,0 +1,146 @@
+genre_groups = {
+    #Rock
+    'rock': 'rock&hardrock',
+    'rock-n-roll': 'rock&hardrock',
+    'hard-rock': 'rock&hardrock',
+    'punk': 'rock&hardrock',
+    'punk-rock': 'rock&hardrock',
+    'grunge': 'rock&hardrock',
+    'psych-rock': 'rock&hardrock',
+    'garage': 'rock&hardrock',
+    'guitar': 'rock&hardrock',
+    'rockabilly': 'rock&hardrock',
+    'alt-rock': 'rock&hardrock',
+    'j-rock': 'rock&hardrock',
+    'ska': 'rock&hardrock',
+
+    #Metal
+    'metal': 'metal',
+    'heavy-metal': 'metal',
+    'death-metal': 'metal',
+    'black-metal': 'metal',
+    'metalcore': 'metal',
+    'grindcore': 'metal',
+    'hardcore': 'metal',
+    'industrial': 'metal',
+    'goth': 'metal',
+    'emo': 'metal',
+
+    #Pop (Mainstream)
+    'pop': 'pop',
+    'power-pop': 'pop',
+    'synth-pop': 'pop',
+    'dance': 'pop',
+    'party': 'pop',
+    'happy': 'pop',
+    'romance': 'pop',
+    'sad': 'pop',
+    'pop-film': 'pop',
+    'show-tunes': 'pop',
+    'comedy': 'pop',
+
+    #Indie / Alternative
+    'indie': 'indie&alt&singersongwriter',
+    'indie-pop': 'indie&alt&singersongwriter',
+    'singer-songwriter': 'indie&alt&singersongwriter',
+    'songwriter': 'indie&alt&singersongwriter',
+    'alternative': 'indie&alt&singersongwriter',
+    'acoustic': 'indie&alt&singersongwriter',
+
+    #Latin
+    'latin': 'latin',
+    'latino': 'latin',
+    'reggaeton': 'latin',
+    'spanish': 'latin',
+
+    #World Pop
+    'world-music': 'world_pop',
+    'k-pop': 'world_pop',
+    'j-pop': 'world_pop',
+    'j-idol': 'world_pop',
+    'mandopop': 'world_pop',
+    'mpb': 'world_pop',
+    'sertanejo': 'world_pop',
+    'pagode': 'world_pop',
+    'forro': 'world_pop',
+    'swedish': 'world_pop',
+    'french': 'world_pop',
+    'german': 'world_pop',
+    'turkish': 'world_pop',
+    'iranian': 'world_pop',
+    'malay': 'world_pop',
+    'indian': 'world_pop',
+    'brazil': 'world_pop',
+    'british': 'world_pop',
+    'j-dance': 'world_pop',
+    'afrobeat': 'world_pop',
+    'reggae': 'world_pop',
+    'cantopop': 'world_pop',
+
+    #EDM (Mainstream Electronic)
+    'edm': 'edm',
+    'electro': 'edm',
+    'dancehall': 'edm',
+    'club': 'edm',
+    'electronic': 'edm',
+    'trip-hop': 'edm',
+
+    #Techno & House (Underground Electronic)
+    'techno': 'techno&house',
+    'hardstyle': 'techno&house',
+    'detroit-techno': 'techno&house',
+    'minimal-techno': 'techno&house',
+    'house': 'techno&house',
+    'deep-house': 'techno&house',
+    'progressive-house': 'techno&house',
+    'trance': 'techno&house',
+    'drum-and-bass': 'techno&house',
+    'dub': 'techno&house',
+    'dubstep': 'techno&house',
+    'idm': 'techno&house',
+    'chicago-house': 'techno&house',
+    'breakbeat': 'techno&house',
+
+    #Soul, Funk & RnB
+    'r-n-b': 'soul&funk&rnb',
+    'soul': 'soul&funk&rnb',
+    'funk': 'soul&funk&rnb',
+    'groove': 'soul&funk&rnb',
+    'gospel': 'soul_funk_rnb',
+
+    #Hip-Hop
+    'hip-hop:': 'hip-hop',
+
+    #Blues & Jazz
+    'jazz': 'blues&jazz',
+    'blues': 'blues&jazz',
+
+    #Classical
+    'piano': 'classical',
+    'opera': 'classical',
+    'classical': 'classical',
+
+    #Country & Folk
+    'country': 'country&folk',
+    'folk': 'country&folk',
+    'honky-tonk': 'country&folk',
+    'bluegrass': 'country&folk',
+
+    #Ethnic Dance
+    'samba': 'ethnic',
+    'salsa': 'ethnic',
+    'tango': 'ethnic',
+
+    #Experimental / Ambient
+    'new-age': 'ambient',
+    'sleep': 'ambient',
+    'study': 'ambient',
+    'chill': 'ambient',
+    'ambient': 'ambient',
+
+    #Children / Disney
+    'kids': 'children',
+    'disney': 'children',
+    'anime': 'children',
+    'children': 'children'
+}
---
+++ src/evaluation_utils.py
@@ -0,0 +1,65 @@
+from IPython.display import display
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import plotly.express as px
+from src.config.genre_groups import genre_groups
+
+
+def plot_songsPerCluster(df, dataset_name):
+    cluster_counts = df[dataset_name].value_counts().sort_index()
+    display(cluster_counts)
+    plt.figure(figsize=(6, 4))
+    cluster_counts.plot(kind='bar', color='skyblue')
+    plt.title("Number of Songs per Cluster")
+    plt.xlabel("Cluster")
+    plt.ylabel("Count")
+    plt.show()
+
+
+def plot_clusters_heatmap(df, dataset_name):
+    genres = df["track_genre"].replace(genre_groups)
+
+    genre_cluster_table = pd.crosstab(df[dataset_name], genres)
+
+    plt.figure(figsize=(18, 6))
+    sns.heatmap(genre_cluster_table, cmap="YlGnBu", linewidths=0.5, annot=True, fmt="d")
+    plt.title("Cluster–Genre Group Distribution (Top Genres)")
+    plt.xlabel("Genre Group")
+    plt.ylabel("Cluster ID")
+    plt.show()
+
+
+def plot_clusters_bubbleplot(df, dataset_name):
+    df["genre_group"] = df["track_genre"].replace(genre_groups)
+
+    genre_cluster_counts = (
+        df.groupby([dataset_name, "genre_group"])
+        .size()
+        .reset_index(name="count")
+    )
+
+    fig = px.scatter(
+        genre_cluster_counts,
+        x=dataset_name,
+        y="genre_group",
+        size="count",
+        color="genre_group",
+        hover_name="genre_group",
+        color_discrete_sequence=px.colors.qualitative.Vivid + px.colors.qualitative.Prism,
+        size_max=70,
+    )
+
+    fig.update_traces(marker=dict(line=dict(width=1, color="DarkSlateGrey")))
+    fig.update_layout(
+        xaxis_title="Cluster ID",
+        yaxis_title="Main Genre Group",
+        plot_bgcolor="black",
+        paper_bgcolor="black",
+        font=dict(color="white"),
+        height=750,
+    )
+
+    fig.show()
